58540e3ea42b651b86cab2e9af2d8dd79c9164bd
Leon Kiesgen <leon.kiesgen@web.de>
2020-09-06 17:15:10 +0200

    added Routing and navigation

--- App.js
+++ App.js
@@ -1,10 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+function RunSession() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Running Screen</Text>
+    </View>
+  );
+}
+
+function AddSession() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Add Section</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Run" component={DetailsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
