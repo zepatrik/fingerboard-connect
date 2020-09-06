ed1003954ffb9e6c5fc7cc747c2d9ad7d630dd83
Leon Kiesgen <leon.kiesgen@web.de>
2020-09-06 18:59:58 +0200

    format

--- App.js
+++ App.js
@@ -7,83 +7,69 @@ import { Ionicons } from '@expo/vector-icons'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 function HomeScreen() {
-    return (
-        <View
-            style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
-        >
-            <Text>Home Screen</Text>
-        </View>
-    )
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  )
 }
 
 function Social() {
-    return (
-        <View
-            style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
-        >
-            <Text>Soical Screen</Text>
-        </View>
-    )
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Soical Screen</Text>
+    </View>
+  )
 }
 
 function AddSession() {
-    return (
-        <View
-            style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
-        >
-            <Text>Add Section</Text>
-        </View>
-    )
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Add Section</Text>
+    </View>
+  )
 }
 
 const Stack = createStackNavigator()
 const Tab = createBottomTabNavigator()
 
 export default function App() {
-    return (
-        <NavigationContainer>
-            <Tab.Navigator
-                screenOptions={({ route }) => ({
-                    tabBarIcon: ({ focused, color, size }) => {
-                        let iconName
-                        if (route.name === 'Home') {
-                            iconName = focused ? 'ios-list' : 'ios-list-box'
-                        } else if (route.name === 'Social') {
-                            iconName = focused ? 'md-share' : 'md-share'
-                        } else if (route.name === 'Add') {
-                            iconName = focused
-                                ? 'ios-add-circle'
-                                : 'ios-add-circle-outline'
-                        }
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName
+            if (route.name === 'Home') {
+              iconName = focused ? 'ios-list' : 'ios-list-box'
+            } else if (route.name === 'Social') {
+              iconName = focused ? 'md-share' : 'md-share'
+            } else if (route.name === 'Add') {
+              iconName = focused ? 'ios-add-circle' : 'ios-add-circle-outline'
+            }
 
-                        // You can return any component that you like here!
-                        return (
-                            <Ionicons
-                                name={iconName}
-                                size={size}
-                                color={color}
-                            />
-                        )
-                    }
-                })}
-                tabBarOptions={{
-                    activeTintColor: 'tomato',
-                    inactiveTintColor: 'gray'
-                }}
-            >
-                <Tab.Screen name="Home" component={HomeScreen} />
-                <Tab.Screen name="Add" component={AddSession} />
-                <Tab.Screen name="Social" component={Social} />
-            </Tab.Navigator>
-        </NavigationContainer>
-    )
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />
+          }
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray'
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Add" component={AddSession} />
+        <Tab.Screen name="Social" component={Social} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 })
