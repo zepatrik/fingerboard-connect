ffed2db54d3f0fdd08f5cac59cfabe37575d17f0
Leon Kiesgen <leon.kiesgen@web.de>
2020-09-06 18:56:25 +0200

    finished tabs screens

--- App.js
+++ App.js
@@ -1,58 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { Ionicons } from '@expo/vector-icons'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
+    return (
+        <View
+            style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+        >
+            <Text>Home Screen</Text>
+        </View>
+    )
 }
 
-function RunSession() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Running Screen</Text>
-    </View>
-  );
+function Social() {
+    return (
+        <View
+            style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+        >
+            <Text>Soical Screen</Text>
+        </View>
+    )
 }
 
 function AddSession() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Add Section</Text>
-    </View>
-  );
+    return (
+        <View
+            style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+        >
+            <Text>Add Section</Text>
+        </View>
+    )
 }
 
-const Stack = createStackNavigator();
-
+const Stack = createStackNavigator()
+const Tab = createBottomTabNavigator()
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Run" component={DetailsScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            <Tab.Navigator
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ focused, color, size }) => {
+                        let iconName
+                        if (route.name === 'Home') {
+                            iconName = focused ? 'ios-list' : 'ios-list-box'
+                        } else if (route.name === 'Social') {
+                            iconName = focused ? 'md-share' : 'md-share'
+                        } else if (route.name === 'Add') {
+                            iconName = focused
+                                ? 'ios-add-circle'
+                                : 'ios-add-circle-outline'
+                        }
+
+                        // You can return any component that you like here!
+                        return (
+                            <Ionicons
+                                name={iconName}
+                                size={size}
+                                color={color}
+                            />
+                        )
+                    }
+                })}
+                tabBarOptions={{
+                    activeTintColor: 'tomato',
+                    inactiveTintColor: 'gray'
+                }}
+            >
+                <Tab.Screen name="Home" component={HomeScreen} />
+                <Tab.Screen name="Add" component={AddSession} />
+                <Tab.Screen name="Social" component={Social} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
