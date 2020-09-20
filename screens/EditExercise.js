b36762c7df0a7b2173dd97d6dcdcd9e43a7cd9a4
zepatrik <patrik@ory.sh>
2020-09-20 19:31:21 +0200

    format

--- screens/EditExercise.js
+++ screens/EditExercise.js
@@ -5,10 +5,10 @@ const FingerBoard = () => (
   <View style={{ width: 100, height: 100, backgroundColor: '#ff0000' }} />
 )
 
-const repetitionType = "repetition"
+const repetitionType = 'repetition'
 const durationType = 'duration'
 
-const styles =  {
+const styles = {
   inputRow: { flexDirection: 'row' },
   textInput: {
     borderWidth: 1,
@@ -17,7 +17,9 @@ const styles =  {
   }
 }
 
-const EditExercise = ({ onSubmit = console.log } = { onSubmit: console.log }) => {
+const EditExercise = (
+  { onSubmit = console.log } = { onSubmit: console.log }
+) => {
   const [notes, setNotes] = useState('asdf')
   const [type, setType] = useState('')
   const [pattern, setPattern] = useState([-1, -1])
@@ -28,18 +30,21 @@ const EditExercise = ({ onSubmit = console.log } = { onSubmit: console.log }) =>
       <FingerBoard />
       <View style={styles.inputRow}>
         <Text>{repetitionType}</Text>
-        <Switch value={type === repetitionType} onValueChange={(v) => setType(v ? repetitionType : durationType)}/>
+        <Switch
+          value={type === repetitionType}
+          onValueChange={(v) => setType(v ? repetitionType : durationType)}
+        />
         <Text>{durationType}</Text>
       </View>
       <View style={styles.inputRow}>
         <Text>Count: </Text>
         <TextInput
           value={count}
-          onChangeText={t => setCount(parseInt(t))}
+          onChangeText={(t) => setCount(parseInt(t))}
           style={styles.textInput}
         />
-        <Button title="+" onPress={() => setCount(c => c + 1)} />
-        <Button title="-" onPress={() => setCount(c => c - 1)} />
+        <Button title="+" onPress={() => setCount((c) => c + 1)} />
+        <Button title="-" onPress={() => setCount((c) => c - 1)} />
       </View>
       <View style={styles.inputRow}>
         <Text>Notes: </Text>
@@ -50,7 +55,10 @@ const EditExercise = ({ onSubmit = console.log } = { onSubmit: console.log }) =>
         />
       </View>
       <View style={styles.inputRow}>
-        <Button title="Save" onPress={() => onSubmit({ notes, [type]: count, pattern })} />
+        <Button
+          title="Save"
+          onPress={() => onSubmit({ notes, [type]: count, pattern })}
+        />
       </View>
     </View>
   )
